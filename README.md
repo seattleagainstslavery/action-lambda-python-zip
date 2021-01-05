a3f29d2cdef0cdabb565fd5aec5d1bfc45ed9ad4
Pete Dunlap <pete@seattleagainstslavery.org>
2021-01-05 15:23:53 -0600

    first attempt to support multiple functions and layers

--- README.md
+++ README.md
@@ -9,26 +9,26 @@ This action automatically installs requirements, zips and deploys the code inclu
 #### Python 3.8 is supported
 
 ### Pre-requisites
-In order for the Action to have access to the code, you must use the `actions/checkout@master` job before it. 
+In order for the Action to have access to the code, you must use the `actions/checkout@master` job before it.
 
 ### Structure
 - Lambda code should be `lambda_function.py`** unless you want to have a customized file name.
 - **Dependencies must be stored in a `requirements.txt`**
 
 ### Environment variables
-Storing credentials as secret is stronly recommended. 
+Storing credentials as secret is stronly recommended.
 
-- **AWS Credentials**  
+- **AWS Credentials**
     `AWS_ACCESS_KEY_ID`, `AWS_SECRET_ACCESS_KEY`, `REGION` are required.
 
 ### Inputs
-- `lambda_region`  
-- `lambda_layer_arn`  
+- `lambda_region`
+- `lambda_layer_arn`
     The ARN for the Lambda layer the dependencies should be pushed to **without the version** (version will be automatically updated on AWS).
-- `lambda_function_name`  
+- `lambda_function_name`
     The Lambda function name. [From the AWS docs](https://docs.aws.amazon.com/cli/latest/reference/lambda/update-function-code.html), it can be any of the following:
-    - Function name - `function-name`  
-    - Function ARN - `arn:aws:lambda:us-west-2:123456789012:function:function-name`  
+    - Function name - `function-name`
+    - Function ARN - `arn:aws:lambda:us-west-2:123456789012:function:function-name`
     - Partial ARN - `123456789012:function:function-name`
 - `requirements_txt`
     The name/path for the `requirements.txt` file. Defaults to `requirements.txt`.
@@ -42,11 +42,11 @@ on:
 jobs:
   build:
     runs-on: ubuntu-latest
-    
+
     steps:
     - uses: actions/checkout@master
     - name: Deploy code to Lambda
-      uses: qubitro/action-lambda-python-zip@v1.0
+      uses: seattleagainstslavery/action-lambda-python-zip@v1.0
       with:
         lambda_layer_arn: 'arn:aws:lambda:eu-central-1:222222222222:layer:layer-name'
         lambda_function_name: ${{ secrets.LAMBDA_FUNCTION_NAME }}
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,8 @@
 
 configure_aws_credentials(){
 	aws configure set aws_access_key_id "${INPUT_AWS_ACCESS_KEY_ID}"
-    aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}"
-    aws configure set default.region "${INPUT_LAMBDA_REGION}"
+  aws configure set aws_secret_access_key "${INPUT_AWS_SECRET_ACCESS_KEY}"
+  aws configure set default.region "${INPUT_LAMBDA_REGION}"
 }
 
 install_zip_dependencies(){
@@ -15,29 +15,41 @@ install_zip_dependencies(){
 
 publish_dependencies_as_layer(){
 	echo "Publishing dependencies as a layer..."
-	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_LAMBDA_LAYER_ARN}" --zip-file fileb://dependencies.zip)
+	local result=$(aws lambda publish-layer-version --layer-name "${INPUT_PIP_LAYER_ARN}" --zip-file fileb://dependencies.zip)
 	LAYER_VERSION=$(jq '.Version' <<< "$result")
 	rm -rf python
 	rm dependencies.zip
 }
 
-publish_function_code(){
-	echo "Deploying the code itself..."
-	zip -r code.zip . -x \*.git\*
-	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --zip-file fileb://code.zip
+publish_custom_layers(){
+  echo "imagine I was publishing custom layers"
+  echo "${INPUT_CUSTOM_LAYERS}"
 }
 
-update_function_layers(){
-	echo "Using the layer in the function..."
-	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTION_NAME}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
+publish_functions(){
+	echo "IMAGE THIS LOOPED USING each of the names"
+  echo "${INPUT_LAMBDA_FUNCTIONS}"
+
+  echo "Deploying the code itself..."
+	zip -r code.zip . -x \*.git\*
+	aws lambda update-function-code --function-name "${INPUT_LAMBDA_FUNCTIONS}" --zip-file fileb://code.zip
+}
+
+update_functions_layers(){
+	echo "IMAGINE THIS LOOPED USING EACH OF THE FUNCTION NAMES AND WIRED UP ALL THE LAYERS"
+  echo "${INPUT_LAMBDA_FUNCTIONS}"
+
+  echo "Using the layer in the function..."
+	aws lambda update-function-configuration --function-name "${INPUT_LAMBDA_FUNCTIONS}" --layers "${INPUT_LAMBDA_LAYER_ARN}:${LAYER_VERSION}"
 }
 
 deploy_lambda_function(){
-    configure_aws_credentials
+  configure_aws_credentials
 	install_zip_dependencies
 	publish_dependencies_as_layer
-	publish_function_code
-	update_function_layers
+  publish_custom_layers
+	publish_functions
+	update_functions_layers
 }
 
 deploy_lambda_function
